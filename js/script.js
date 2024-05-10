38dab7bedf743695447a6aa70c7921de2d1e399a
firmturmeric <152993980+firmturmeric@users.noreply.github.com>
2024-05-10 15:09:41 +0000

    changes

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function changeSlide(direction) {
+  currentSlide += direction;
+  
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  } else if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  
+  slides.forEach((slide, index) => {
+    if (index === currentSlide) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
